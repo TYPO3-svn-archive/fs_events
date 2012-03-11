d3010b6d8bc872fa1c536ecfcd36c02c7f36e403
freeshsolutions <freeshsolutions@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-11 13:41:22 +0000

    working on coordinates for googlemaps
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fs_events/trunk@59121 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -54,7 +54,7 @@ CREATE TABLE tx_fsevents_domain_model_event (
 	event_start_date int(11) DEFAULT '0' NOT NULL,
 	event_end_date int(11) DEFAULT '0' NOT NULL,
 	price varchar(255) DEFAULT '' NOT NULL,
-	ticket_link varchar(255) DEFAULT '' NOT NULL,
+	tickets  int(11) unsigned DEFAULT '0' NOT NULL,
 	category int(11) unsigned DEFAULT '0' NOT NULL,
 	location int(11) unsigned DEFAULT '0',
 	status int(11) unsigned DEFAULT '0',
@@ -190,4 +190,63 @@ CREATE TABLE tx_fsevents_event_category_mm (
 
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign)
+);
+
+#
+# Table structure for table 'tx_fsevens_domain_model_tickets'
+#
+CREATE TABLE tx_fsevents_domain_model_tickets (
+
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	item int(11) unsigned DEFAULT '0' NOT NULL,
+
+	type int(11) DEFAULT '0' NOT NULL,
+	url varchar(255) DEFAULT '' NOT NULL,
+	tel varchar(255) DEFAULT '' NOT NULL,
+	street varchar(255) DEFAULT '' NOT NULL,
+	zip int(11) DEFAULT '0' NOT NULL,
+	city varchar(255) DEFAULT '' NOT NULL,
+	country varchar(255) DEFAULT '' NOT NULL,
+	text varchar(255) DEFAULT '' NOT NULL,
+	formurl varchar(255) DEFAULT '' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(255) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+	t3_origuid int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+	KEY language (l10n_parent,sys_language_uid)
+
+);
+
+#
+# Table structure for table 'tx_fsexttest_domain_model_tickets'
+#
+CREATE TABLE tx_fsevents_domain_model_irrefield (
+
+	event  int(11) unsigned DEFAULT '0' NOT NULL,
+
 );
